f03d8c86f2eff244c84bb58ba51e55e18027e0c0
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-07-03 15:55:38 -0400

    init: intro to node.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const {generateRandomNumber, 
+        celsiusToFahrenheit
+        } = require('./utils');
+
+const { milesToKilometers } = require('./utils')
+
 console.log('Hello World!')
+console.log(`Here is random number: ${generateRandomNumber()}`)
+console.log(` Celsius will be ${celsiusToFahrenheit(0)} Fahrenheit`)
+console.log(`Miles to kilometers conversion: ${milesToKilometers(10)}`)
---
+++ utils.js
@@ -0,0 +1,16 @@
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9) / 5 + 32;
+}
+
+function milesToKilometers(miles) {
+    return miles * 1.60934;
+}
+
+module.exports = {
+    generateRandomNumber,
+    celsiusToFahrenheit,
+    milesToKilometers};
