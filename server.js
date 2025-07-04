1c3f3d180902f5c593518e76348edf0cb7f8e3d0
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-07-04 13:47:43 -0400

    feat: get file and directory paths

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import http from 'http'
+import url from 'url'
+import path from 'path'
 const PORT = process.env.PORT;
+
+//Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname)
 
 const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/html');
@@ -8,7 +16,9 @@ const server = http.createServer((req, res) => {
     // console.log(req.url);
     // console.log(req.method);
 
-    if (req.url === '/') {
+    try {
+        if (req.method === 'GET') {
+            if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html'});
         res.end('<h1>Home Page</h1>');
     } else if (req.url === '/about') {
@@ -16,8 +26,17 @@ const server = http.createServer((req, res) => {
         res.end('<h1>About Page</h1>');
     } else {
         res.writeHead(404, { 'Content-Type': 'text/html'});
-        res.end('<h1>Not Found</h1>')
+        res.end('<h1>Not Found</h1>');
     }
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain'});
+        res.end('Server Error')
+    }
+
+    
 
     // res.writeHead(200, { 'Content-Type': 'text/plain'})
     // res.end('<h1>Hello people</h1>');
