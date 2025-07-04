6f52992c7338ec2f0e0493e8f8d44cc14cd58862
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-07-04 10:57:11 -0400

    feat: simple routing functionality

--- server.js
+++ server.js
@@ -5,8 +5,22 @@ const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/html');
     // res.statusCode = 404;
     // res.write('Hello World! This is cool!')
-    res.writeHead(200, { 'Content-Type': 'text/plain'})
-    res.end('<h1>Hello people</h1>');
+    // console.log(req.url);
+    // console.log(req.method);
+
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end('<h1>Home Page</h1>');
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end('<h1>About Page</h1>');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html'});
+        res.end('<h1>Not Found</h1>')
+    }
+
+    // res.writeHead(200, { 'Content-Type': 'text/plain'})
+    // res.end('<h1>Hello people</h1>');
 })
 
 server.listen(PORT, () => {
